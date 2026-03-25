09dfccdef53f4923a5ffb9a594e101c60dc0ed1d
XinGclover <cindygao7809@gmail.com>
2026-03-25 13:26:18 +0100

    remove group

--- models/marts/dimensions/dim_topics.sql
+++ models/marts/dimensions/dim_topics.sql
@@ -14,7 +14,6 @@ deduped as (
         reply_count,
         group_id,
         key_word,
-        insert_time,
         source_name,
         row_number() over (
             partition by topic_id
@@ -32,7 +31,6 @@ select
     reply_count,
     group_id,
     key_word,
-    insert_time,
     source_name
 from deduped
 where rn = 1
